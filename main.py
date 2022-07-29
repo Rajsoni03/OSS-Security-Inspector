03be2ec5bbfc90344b8e5d39a94e3ead8c05359d
Raj - Jetson Nano <rajksoni029@gmail.com>
2022-07-30 00:46:09 +0530

    git test done

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify, request, make_response
 import git
 import os
 import re
-from scan import scan_repo
+from scan import scan_repo, scan_dependency
 app = Flask(__name__) 
 
 
@@ -32,8 +32,61 @@ def git_scan_link():
 				path = os.path.join(os.path.join(os.getcwd(), 'repo'), reponame)
 				# scan
 				params['report'] = scan_repo(path)
+				
+				# delete repo
+				# os.system(f"rm -rf {path}")
+			params['status'] = True
+		except:
+			params['status'] = False	
+	return jsonify(params)
+
+@app.route('/depend-scan', methods=['POST'])
+def depend_scan():
+	params = {'status': False, 'report' : None}
+	if (request.method == 'POST'):
+		try:
+			url = request.form.get('repolink')
+			if (url != None) and (url.find("github.com") != -1):
+				arr = url.split('/')
+				username = arr[-2]
+				reponame = arr[-1]
+				path = os.path.join(os.path.join(os.getcwd(), 'repo'), reponame)
+				# scan
+				# params['depenency_report'] = scan_dependency(path)
+
 				# delete repo
 				os.system(f"rm -rf {path}")
+			params['status'] = True
+		except:
+			params['status'] = False	
+	return jsonify(params)
+
+
+@app.route("/py-scan")
+def git_scan():
+	params = {'name':'Raj'}
+	return render_template('py_scan.html', params = params)
+
+
+@app.route('/git-scan-link', methods=['POST'])
+def git_scan_link():
+	params = {'status': False, 'report' : None}
+	if (request.method == 'POST'):
+		try:
+			url = request.form.get('repolink')
+			if (url != None) and (url.find("github.com") != -1):
+				arr = url.split('/')
+				username = arr[-2]
+				reponame = arr[-1]
+				url = f"https://github.com/{username}/{reponame}"
+				git.Git('./repo').clone(url)
+
+				path = os.path.join(os.path.join(os.getcwd(), 'repo'), reponame)
+				# scan
+				params['report'] = py_repo(path)
+				
+				# delete repo
+				# os.system(f"rm -rf {path}")
 			params['status'] = True
 		except:
 			params['status'] = False	
--- scan.py
+++ scan.py
@@ -1,6 +1,5 @@
 import os
 import re
-import bandit
 
 def scan_repo(path):
 	stream = os.popen(f'bandit -r {path}')
@@ -15,8 +14,8 @@ def scan_repo(path):
 
 	length = 0
 	for i in bySeverity.keys():
-	    bySeverity[i] = int(re.search(f"{i}.*", report_severity)[0].split(' ')[1])
-	    byConfidence[i] = int(re.search(f"{i}.*", report_confidence)[0].split(' ')[1])
+	    bySeverity[i] = int(re.search(f"{i}.*", report_severity)[0].split('.')[-2].split(' ')[-1])
+	    byConfidence[i] = int(re.search(f"{i}.*", report_confidence)[0].split('.')[-2].split(' ')[-1])
 	    length += bySeverity[i]
 	    
 	issueList = []
@@ -25,8 +24,7 @@ def scan_repo(path):
 	    issue['Issue'] = re.search("Issue.*", i)[0][re.search("Issue.*", i)[0].find(']')+1:]
 	    issue['Severity'] = re.search("Severity.*", i)[0].split(' ')[1]
 	    issue['Confidence'] = re.search("Severity.*", i)[0].split(' ')[-1]
-	    issue['CWE'] = re.search("CWE.*", i)[0][5:]
-	    issue['Link'] = re.search("CWE.*", i)[0][re.search("CWE.*", i)[0].find('(')+1:-1]
+	    issue['Link'] = re.search("More Info:.*", i)[0][10:]
 	    issue['Location'] = re.search("Location.*", i)[0][10:]
 	    issue['Code'] = i[re.search("More Info.*", i).span()[1]+1:-1]
 	    issueList.append(issue)
@@ -38,3 +36,77 @@ def scan_repo(path):
 		'issueList'    : issueList
 	}
 	return data
+
+def scan_dependency(path):
+	soft = "/home/jetson/Desktop/FlipKart/OSS-Security-Inspector/grype"
+	stream = os.popen(f'{soft} {path}')
+	output = stream.read()
+
+	dependencyList = []
+	length = 0
+
+	for i in output.split('\n')[1:-1]:
+	    string = [j for j in i.split(' ') if j!='']
+	    dependency = {}
+	    dependency['Name'] = string[0]
+	    dependency['Installled'] = string[1]
+	    if (string[2][0] in '0123456789'):
+	    	dependency['Fixed-In'] = string[2]
+	    	dependency['Type'] = string[3]
+	    	dependency['Vulnerability'] = string[4]
+	    	dependency['Severity'] = string[5]
+	    else:
+	    	dependency['Fixed-In'] = ''
+	    	dependency['Type'] = string[2]
+	    	dependency['Vulnerability'] = string[3]
+	    	dependency['Severity'] = string[4]
+
+	    dependencyList.append(dependency)
+	    length+=1
+
+	data = {
+		'dependencyList' : dependencyList,
+		'length' : length
+	}
+	return data
+
+def scan_py(path):
+	stream = os.popen(f'bandit -r {path}')
+	output = stream.read()
+	report = output[output.find('>>'):].split('--------------------------------------------------')[-1]
+	
+	bySeverity =   {'Undefined':0, 'Low':0, 'Medium':0, 'High':0 }
+	byConfidence = {'Undefined':0, 'Low':0, 'Medium':0, 'High':0 }
+
+	report_severity = report[report.find('(by severity)'):report.find('(by confidence)')]
+	report_confidence = report[report.find('(by confidence)'):]
+
+	length = 0
+	for i in bySeverity.keys():
+	    bySeverity[i] = int(re.search(f"{i}.*", report_severity)[0].split('.')[-2].split(' ')[-1])
+	    byConfidence[i] = int(re.search(f"{i}.*", report_confidence)[0].split('.')[-2].split(' ')[-1])
+	    length += bySeverity[i]
+	    
+	issueList = []
+	for i in output.split('--------------------------------------------------')[:-1]:
+	    issue = {}
+	    issue['Issue'] = re.search("Issue.*", i)[0][re.search("Issue.*", i)[0].find(']')+1:]
+	    issue['Severity'] = re.search("Severity.*", i)[0].split(' ')[1]
+	    issue['Confidence'] = re.search("Severity.*", i)[0].split(' ')[-1]
+	    issue['Link'] = re.search("More Info:.*", i)[0][10:]
+	    issue['Location'] = re.search("Location.*", i)[0][10:]
+	    issue['Code'] = i[re.search("More Info.*", i).span()[1]+1:-1]
+	    issueList.append(issue)
+
+	data = {
+		'length'       : length,
+		'bySeverity'   : bySeverity,
+		'byConfidence' : byConfidence,
+		'issueList'    : issueList
+	}
+	return data
+
+def download_pypi(name):
+	stream = os.popen(f"pip download {name} --no-deps --cache-dir='./repo'")
+	output = stream.read()
+	report = output[output.find('>>'):].split('--------------------------------------------------')[-1]
