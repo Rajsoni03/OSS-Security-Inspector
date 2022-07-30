3854d703470f50ab570f3ebefe0301c49708048b
Raj - Jetson Nano <rajksoni029@gmail.com>
2022-07-31 00:28:09 +0530

    almost done

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, jsonify, request, make_response
+from flask import Flask, render_template, jsonify, request, make_response, flash
 import git
 import os
 import re
-from scan import scan_repo, scan_dependency
+from werkzeug.utils import secure_filename
+from scan import scan_dependency, download_pypi, scan_py
+import glob
 app = Flask(__name__) 
-
 
 @app.route("/") 
 def home_view():
@@ -31,7 +32,7 @@ def git_scan_link():
 
 				path = os.path.join(os.path.join(os.getcwd(), 'repo'), reponame)
 				# scan
-				params['report'] = scan_repo(path)
+				params['report'] = scan_py(path)
 				
 				# delete repo
 				# os.system(f"rm -rf {path}")
@@ -52,48 +53,142 @@ def depend_scan():
 				reponame = arr[-1]
 				path = os.path.join(os.path.join(os.getcwd(), 'repo'), reponame)
 				# scan
-				# params['depenency_report'] = scan_dependency(path)
+				params['depenency_report'] = scan_dependency(path)
 
 				# delete repo
 				os.system(f"rm -rf {path}")
-			params['status'] = True
+				params['status'] = True
 		except:
 			params['status'] = False	
 	return jsonify(params)
 
-
 @app.route("/py-scan")
-def git_scan():
+def py_scan():
 	params = {'name':'Raj'}
 	return render_template('py_scan.html', params = params)
 
-
-@app.route('/git-scan-link', methods=['POST'])
-def git_scan_link():
+@app.route('/py-scan-link', methods=['POST'])
+def py_scan_link():
 	params = {'status': False, 'report' : None}
 	if (request.method == 'POST'):
 		try:
-			url = request.form.get('repolink')
-			if (url != None) and (url.find("github.com") != -1):
-				arr = url.split('/')
-				username = arr[-2]
-				reponame = arr[-1]
-				url = f"https://github.com/{username}/{reponame}"
-				git.Git('./repo').clone(url)
+			modulename = request.form.get('modulename')
+			if modulename != None:
+				if download_pypi(modulename):
+					path = os.path.join(os.path.join(os.getcwd(), './pyrepo'))
+					# scan
+					params['report'] = scan_py(path)
+					
+					# delete pyrepo
+					#os.system(f"rm -rf {path}")
+					params['status'] = True
+		except:
+			params['status'] = False	
+	return jsonify(params)
 
-				path = os.path.join(os.path.join(os.getcwd(), 'repo'), reponame)
-				# scan
-				params['report'] = py_repo(path)
+@app.route('/py_depend-scan', methods=['POST'])
+def py_depend_scan():
+	params = {'status': False, 'report' : None}
+	if (request.method == 'POST'):
+		try:
+			modulename = request.form.get('modulename')
+			if modulename != None:			
+				path = os.path.join(os.path.join(os.getcwd(), './pyrepo'))
+				filepath = glob.glob(f"{path}/*/METADATA")[0]
 				
+				# read dependency from metadata 
+				f = open(filepath, 'r')
+				meta = f.read()
+				f.close()
+				requires = re.findall("Requires-Dist.*", meta)
+				
+				# check dependency version 
+				dependency_list = ''
+				for i in requires:
+				    row = i.split(' ') 
+				    depd = row[1]
+				    try:
+				        depd += row[2][1:-1] + '\n'
+				    except:
+				        depd += '\n'
+				    dependency_list+=depd
+
+				# copy dependency to create requirements file
+				f = open(f'{path}/requirements.txt', 'w')
+				f.write(dependency_list)
+				f.close()
+
+				# scan
+				params['depenency_report'] = scan_dependency(path)
+
 				# delete repo
-				# os.system(f"rm -rf {path}")
-			params['status'] = True
+				os.system(f"rm -rf {path}")
+				params['status'] = True
 		except:
 			params['status'] = False	
 	return jsonify(params)
 
 
-@app.route('/upload', methods=['POST'])
-def upload():
-	params = {}
+@app.route("/file-scan")
+def file_scan():
+	params = {'name': 'Raj'}
+	return render_template('file_scan.html', params = params)
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'zip', 'xz', '7z'}
+
+@app.route("/file-scan-link", methods=['POST'])
+def file_scan_link():
+	params = {'status': False, 'report' : None}
+	if 'formFile' not in request.files:
+		return jsonify(params)    
+	file = request.files['formFile']
+	path = os.path.join(os.path.join(os.getcwd(), 'tempfiles'))
+
+	if file.filename != '' and file and allowed_file(file.filename):
+		os.system(f'mkdir {path}')
+		filename = secure_filename(file.filename)
+		filepath = os.path.join(path, filename)
+		file.save(filepath)
+		
+		filextn = filename.rsplit('.', 1)[1].lower()
+		if filextn == 'zip':
+			os.system(f'unzip {filepath} -d {path}')
+		elif filextn == 'xz':
+			os.system(f'tar -xf {filepath} -C {path}')
+		elif filextn == '7z':
+			os.system(f'7z x {filepath} -o{path}')	
+		os.system(f"rm -rf {filepath}")
+
+		# scan
+		params['report'] = scan_py(path)
+
+		# os.system(f"rm -rf {path}")			
+		params['status'] = True
+	else:
+		params['status'] = False
+	return jsonify(params)
+
+
+@app.route("/file-depend-scan", methods=['POST'])
+def file_depend_scan():
+	params = {'status': False, 'depenency_report' : None}
+	if 'formFile' not in request.files:
+		return jsonify(params)    
+	file = request.files['formFile']
+	path = os.path.join(os.path.join(os.getcwd(), 'tempfiles'))
+
+	if file.filename != '' and file:
+		os.system(f'mkdir {path}')
+		filename = secure_filename(file.filename)
+		filepath = os.path.join(path, filename)
+
+		# scan
+		params['depenency_report'] = scan_dependency(path)
+
+		os.system(f"rm -rf {path}")			
+		params['status'] = True
+	else:
+		params['status'] = False
 	return jsonify(params)
--- scan.py
+++ scan.py
@@ -1,11 +1,20 @@
 import os
 import re
 
-def scan_repo(path):
+def scan_py(path):
 	stream = os.popen(f'bandit -r {path}')
 	output = stream.read()
+	if output.find('>>') == -1:
+		data = {
+			'length'       : 0,
+			'bySeverity'   : {'Undefined':0, 'Low':0, 'Medium':0, 'High':0 },
+			'byConfidence' : {'Undefined':0, 'Low':0, 'Medium':0, 'High':0 },
+			'issueList'    : []
+		}
+		return data
+
 	report = output[output.find('>>'):].split('--------------------------------------------------')[-1]
-	
+
 	bySeverity =   {'Undefined':0, 'Low':0, 'Medium':0, 'High':0 }
 	byConfidence = {'Undefined':0, 'Low':0, 'Medium':0, 'High':0 }
 
@@ -70,43 +79,14 @@ def scan_dependency(path):
 	}
 	return data
 
-def scan_py(path):
-	stream = os.popen(f'bandit -r {path}')
-	output = stream.read()
-	report = output[output.find('>>'):].split('--------------------------------------------------')[-1]
-	
-	bySeverity =   {'Undefined':0, 'Low':0, 'Medium':0, 'High':0 }
-	byConfidence = {'Undefined':0, 'Low':0, 'Medium':0, 'High':0 }
-
-	report_severity = report[report.find('(by severity)'):report.find('(by confidence)')]
-	report_confidence = report[report.find('(by confidence)'):]
-
-	length = 0
-	for i in bySeverity.keys():
-	    bySeverity[i] = int(re.search(f"{i}.*", report_severity)[0].split('.')[-2].split(' ')[-1])
-	    byConfidence[i] = int(re.search(f"{i}.*", report_confidence)[0].split('.')[-2].split(' ')[-1])
-	    length += bySeverity[i]
-	    
-	issueList = []
-	for i in output.split('--------------------------------------------------')[:-1]:
-	    issue = {}
-	    issue['Issue'] = re.search("Issue.*", i)[0][re.search("Issue.*", i)[0].find(']')+1:]
-	    issue['Severity'] = re.search("Severity.*", i)[0].split(' ')[1]
-	    issue['Confidence'] = re.search("Severity.*", i)[0].split(' ')[-1]
-	    issue['Link'] = re.search("More Info:.*", i)[0][10:]
-	    issue['Location'] = re.search("Location.*", i)[0][10:]
-	    issue['Code'] = i[re.search("More Info.*", i).span()[1]+1:-1]
-	    issueList.append(issue)
-
-	data = {
-		'length'       : length,
-		'bySeverity'   : bySeverity,
-		'byConfidence' : byConfidence,
-		'issueList'    : issueList
-	}
-	return data
 
 def download_pypi(name):
-	stream = os.popen(f"pip download {name} --no-deps --cache-dir='./repo'")
+	path = os.path.join(os.path.join(os.getcwd(), './pyrepo'))
+	stream = os.popen(f'pip install {name} --no-deps --target="{path}"')
 	output = stream.read()
-	report = output[output.find('>>'):].split('--------------------------------------------------')[-1]
+	msg = output.split('\n')[-2].split(' ')[0]
+	if msg == 'Successfully':
+	    return True
+	return False
+
+    	
